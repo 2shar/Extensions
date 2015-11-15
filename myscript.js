b95638b55b2647ca333db4cc41171589397518c6
2shar <tpatel@lsoc.org>
2015-11-15 16:10:45 -0600

    First Commit

---
+++ myscript.js
@@ -0,0 +1,40 @@
+alert("Hey!"); 
+alert("Whats up?"); 
+alert("Hello?"); 
+var name = prompt("Whats your name?"); 
+alert("Hi, " + name + "."); 
+alert("So, " + name + ", hows life?"); 
+alert("Yeah, I thought so."); 
+alert("Am I annoying you?"); 
+alert("Really?"); 
+alert("Ok, then."); 
+alert("Are you still there?"); 
+alert("Am I still annoying you?"); 
+alert("How about now?");
+alert("Now?");  
+alert("Now?");
+alert("Are you mad?");
+alert("You look mad.");
+alert("You really want to get rid of me, dont you?");
+alert("Fine I will leave, after 10 more pages.");
+alert("9");
+alert("8");
+alert("7");
+alert("6");
+alert("5");
+alert("4");
+alert("3");
+alert("2");
+alert("1");
+alert("Psych!");
+alert("Haha!");
+alert("Fine.");
+alert("I guess you REALLY want to get rid of me.");
+alert("Ok, Bye!");
+alert("For now...");
+
+
+document.querySelectorAll("div").style.color = "red";
+
+
+
